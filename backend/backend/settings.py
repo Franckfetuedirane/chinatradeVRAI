d9c3a42e3ffa79f9343dc76d2bb8984303e70a9b
Franck Fetue <franckfetuef@gmail.com>
2025-12-16 01:04:08 +0100

    venv et setting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,12 +1,18 @@
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = "changeme-for-dev"
+# SECRET_KEY = "changeme-for-dev"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = True
+DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+
+# DEBUG = True
+
+# ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
