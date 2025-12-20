e3e1d6547210c2152aed0687fd5a4aaed123aa34
Franck Fetue <franckfetuef@gmail.com>
2025-12-20 23:09:18 +0100

    images

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,8 +1,11 @@
 from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
-
+from django.shortcuts import render
 
 class ProductListAPIView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+def manage_products(request):
+    return render(request, "products/products_list.html")
